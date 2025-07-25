c8996886b2ecbf0f78160ae9bf4dd52645817669
Yadavperson <shyamsundar12345676@gmail.com>
2025-07-25 19:40:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -27,7 +27,7 @@ DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 60))
 LOGGER_ID = int(getenv("LOGGER_ID", None))
 
 # Get this value from on Telegram by /id
-OWNER_ID = int(getenv("OWNER_ID", 8142914021))
+OWNER_ID = int(getenv("OWNER_ID", 8093741093))
 
 ## Fill these variables if you're deploying on heroku.
 # Your heroku app name
@@ -44,8 +44,8 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/+NPO0XrGK3kM1Mzc9")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/AnneBella_gc")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/VirtualNumbersByAnne")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/AnneBellakgc")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", True))
